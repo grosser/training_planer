18090dc6acdf054fb586e3c572de1f50492a8e02
grosser <grosser.michael@gmail.com>
2012-02-09 07:50:55 -0800

    make it work with default config

--- config/initializers/cfg.rb
+++ config/initializers/cfg.rb
@@ -1,2 +1,10 @@
 require 'yaml'
-CFG = YAML.load(ERB.new((IO.read('config/config.yml'))).result)[Rails.env].with_indifferent_access
+
+if File.exist?('config/config.yml')
+  config = IO.read('config/config.yml')
+else
+  warn "USING EXAMPLE CONFIG, please customize"
+  config = IO.read('config/config.example.yml')
+end
+
+CFG = YAML.load(ERB.new((config)).result)[Rails.env].with_indifferent_access
